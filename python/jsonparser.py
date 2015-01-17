bff1dd4af0e6dae65beb2728c32e57cb883b5934
peter mitrano <mitranopeter@gmail.com>
2015-01-17 13:10:08 -0500

    added dust

--- python/jsonparser.py
+++ python/jsonparser.py
@@ -2,7 +2,7 @@ import json
 import pprint
 import random
 
-text=["hack", "pennapps", "peter", "joe", "node.js", "rohan", "ryan"]
+text=["hack", "pennapps", "cloud", "service", "node.js", "html5", "continuous integration","I'd like to inteject, that thing you've been referring to as linux is actually GNU linux, or as I've taken to calling it, GNU + Linux","webscale","scalability","API","cross-platform","Rubber-Duck Debugging","dev-ops","github", "software", "programming", "is", "the", "cool", "doge", "bitcoin", "so", "very"]
 
 def get_text_of_width(w,h):
     div_text=""
@@ -15,7 +15,10 @@ def create_div(rect, offset=(0,0)):
     if (rect['type']=='text'):
         div_string+=get_text_of_width(rect['w'],rect['h'])
     elif (rect['type']=='image'):
-        div_string+="<img alt='cats are awesome' src=http://placekitten.com/"+str(rect['w'])+"/"+str(rect['h'])+" width="+str(rect['w'])+" height="+str(rect['h'])+"/>"
+        div_string+="<img alt='cats are awesome' src='http://placekitten.com/g/"+str(rect['w'])+"/"+str(rect['h'])+"' width="+str(rect['w'])+" height="+str(rect['h'])+" >"
+
+    if (rect['glyph']!=None):
+        div_string+="<a href='http://104.131.20.236/page_"+str(rect['glyph'])+"' alt='woah, a hyperlink'>go to page "+str(rect['glyph'])+"</a>"
 
     for child in rect['children']:
         div_string+=create_div(child)
@@ -46,18 +49,16 @@ if __name__ == "__main__":
 
     # read python output of opencv
     pages = [
-    			{'x': 0, 'y': 0, 'w': 1000, 'h': 900, 'type': '', 'glyph': '', 'children': [
-    				{'x': 0, 'y': 0, 'w': 1000, 'h': 40, 'type': 'text', 'glyph': '1', 'children': []}, 
-    				{'x': 400, 'y': 200, 'w': 200, 'h': 400, 'type': '', 'glyph': '', 'children': [
-        				{'x': 0, 'y': 0, 'w': 100, 'h': 400, 'type': 'text', 'glyph': '', 'children': []}, 
-        				{'x': 100, 'y': 0, 'w': 100, 'h': 400, 'type': 'image', 'glyph': '', 'children': []}
+    			{'x': 0, 'y': 0, 'w': 1000, 'h': 900, 'type': '', 'glyph': None, 'children': [
+    				{'x': 0, 'y': 0, 'w': 1000, 'h': 40, 'type': 'text', 'glyph': 1, 'children': []}, 
+    				{'x': 400, 'y': 200, 'w': 600, 'h': 400, 'type': '', 'glyph': None, 'children': [
+        				{'x': 0, 'y': 0, 'w': 300, 'h': 400, 'type': 'text', 'glyph': None, 'children': []}, 
+        				{'x': 300, 'y': 0, 'w': 300, 'h': 400, 'type': 'image', 'glyph': None, 'children': []}
         			]}
         		]}, 
         		{'x': 0, 'y': 0, 'w': 1000, 'h': 900, 'type': '', 'glyph': '', 'children': [
-					{'x': 0, 'y': 0, 'w': 1000, 'h': 100, 'type': 'text', 'glyph': '0', 'children': []}
+					{'x': 0, 'y': 0, 'w': 1000, 'h': 100, 'type': 'text', 'glyph': 0, 'children': []}
         		]}]
-
-    pp = pprint.PrettyPrinter(indent=3)
 
     pageNum = 0
     for page in pages:
@@ -79,12 +80,25 @@ if __name__ == "__main__":
         html_page = "<html>" + parse_header(header) + parse_body(body) + parse_footer(footer) + "</html>"
 
         if (page == pages[0]):
-            filename="index.html"
+            dust_filename="public/templates/index.dust"
+            js_filename="controllers/index.js"
+            js_path="/"
+            render_path="index"
         else :
-            filename="page_"+str(pageNum)+".html"
+            dust_filename="public/templates/page_"+str(pageNum)+".dust"
+            js_filename="controllers/page_"+str(pageNum)+".js"
+            js_path="page_"+str(pageNum)
+            render_path="page_"+str(pageNum)
 
-        pp.pprint(html_page);
-        f = open(filename,'w');
-        f.write(html_page);
+        f_dust = open(dust_filename,'w')
+        f_dust.write("{>'layouts/master' /}")
+        f_dust.write("{<body}")
+        f_dust.write(html_page)
+        f_dust.write("{/body}")
+
+        f_js = open(js_filename,'w')
+        j_s.write("'use strict;'")
+        j_s.write("module.exports = function (app) { app.get('" + js_path + "', function(req,res){ res.render('" + render_path +"');});};")
+
         pageNum+=1
 
