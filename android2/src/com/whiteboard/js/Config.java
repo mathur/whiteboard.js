598f8cff68ff00c58627758a35f87b07f19c5755
Rohan Mathur <rohan@rmathur.com>
2015-01-16 22:22:48 -0500

    Awkward... delete second copy of code
    Signed-off-by: Rohan Mathur <rohan@rmathur.com>

--- android2/src/com/whiteboard/js/Config.java
+++
@@ -1,9 +0,0 @@
-package com.whiteboard.js;
-
-public class Config {
-	// File upload url (replace the ip with your server address)
-	public static final String FILE_UPLOAD_URL = "http://104.131.15.182/fileUpload.php";
-	
-	// Directory name to store captured images
-    	public static final String IMAGE_DIRECTORY_NAME = "whiteboardjs";
-}
