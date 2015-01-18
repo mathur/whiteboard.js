1754b2b73b642523495421970c82007419db84f2
Rohan Mathur <rohan@rmathur.com>
2015-01-17 19:07:43 -0500

    Add portion to execute python after PHP upload
    Signed-off-by: Rohan Mathur <rohan@rmathur.com>

--- www/fileUpload.php
+++ www/fileUpload.php
@@ -3,7 +3,6 @@
 echo $_FILES['image']['name'] . '<br/>';
 
 $target_path = "files/";
-$current_time = time();
 
 $target_path = $target_path . basename($_FILES['image']['name']);
 
@@ -15,12 +14,10 @@ try {
     echo "The file " . basename($_FILES['image']['name']) .
     " has been uploaded";
 
-    $command = escapeshellcmd('python /root/test.py ' . $current_time);
-	$output = shell_exec($command);
-
-	throw new Exception($current_time);
+    $command = escapeshellcmd('python /root/jsonparser.py');
+    $output = shell_exec($command);
 
 } catch (Exception $e) {
-    die('' . $e->getMessage());
+    die('An error occurred:' . $e->getMessage());
 }
 ?>
